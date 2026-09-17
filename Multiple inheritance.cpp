96a1f8ad93e1afb6feab8551624a83ce27f969de
aaryadagade-web <aaryadagade177@gmail.com>
2026-09-17 13:10:50 +0530

    Add files via upload

---
+++ Multiple inheritance.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class student
+{
+	protected:
+		int sid;
+	public:
+		void acc()
+		{
+			cout<<"Enter the student id:";
+			cin>>sid;
+		}
+};
+
+class sports
+{
+	protected:
+		int prn;
+	public:
+		void acc3()
+		{
+			cout<<"Enter prn:";
+			cin>>prn;
+		}
+};
+class fees:public student,public sports
+{
+	private:
+		int amt;
+	public:
+		void acc2()
+		{
+			cout<<"Enter the fees:";
+			cin>>amt;
+		}
+		void disp()
+		{
+			cout<<"Details"<<endl;
+			cout<<sid<<endl<<amt<<endl<<prn<<endl;
+		}
+};
+int main()
+{
+	fees obj;
+	obj.acc();
+	obj.acc2();
+	obj.acc3();
+	obj.disp();
+	return 0;
+}
